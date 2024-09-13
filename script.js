07b5a16623816524fdaa9756d7f31e52025dd222
Valeriia Sharybina <valeriastanis@gmail.com>
2024-09-13 17:22:29 +0200

    added js file with debugging task, homework 2.4.4

---
+++ script.js
@@ -0,0 +1,10 @@
+const calculateBonus = (a, b) => {
+    let bonus;
+    const sum = a + b;
+    debugger; //здесь проверяется текущее значения sum 
+    sum > 50 ? (bonus = 50) : (bonus = sum);
+    debugger; // здесь проверяется значение bonus перед return
+    return bonus;
+    };
+
+    calculateBonus(45, 25);
